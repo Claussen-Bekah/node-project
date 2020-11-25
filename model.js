8ef1644d7f963cca8d65b2e9f0523210905a0510
Bekah <beclau@byui.edu>
2020-11-24 21:59:34 -0700

    added anagram check

--- model.js
+++ model.js
@@ -8,7 +8,6 @@ const pool = new Pool({connectionString: connectionString});
 function getData(callback, id){
 
 
-
     let sql = "SELECT words.word, hints.hint FROM hints JOIN words ON hints.word_id=words.id WHERE hints.word_id =" + id;
 
     pool.query(sql, function(err, result) {
@@ -23,23 +22,6 @@ function getData(callback, id){
 })
 }
 
-
-// function getHint(callback, id){
-
-
-//     let sql = "SELECT hint FROM hints WHERE word_id=" + id;
-
-//     pool.query(sql, function(err, result) {
-//     if (err) {
-//         console.log("Error in query: ")
-//         console.log(err);
-//     }
- 
-
-//     callback(result.rows);
- 
-// })
-// }
 
 module.exports = {
     getData: getData,
--- public/event.js
+++ public/event.js
@@ -1,5 +1,11 @@
 function getWord() {
-    $.get("/newWord", function(result){
+    let hint = document.getElementById('hint');
+
+    if (!hint.classList.contains('hidden')) {
+        hint.classList.add('hidden');
+    }
+
+    $.get("/newWord", function (result) {
 
         console.log(result);
         let div = document.getElementById('word');
@@ -8,15 +14,52 @@ function getWord() {
         let hint = document.getElementById('hint');
         hint.innerHTML = result[0].hint;
 
-
-
     })
 
 
 }
 
-function getHint(){
+function getHint() {
     document.getElementById('hint').classList.toggle('hidden');
 
 }
 
+function checkAnagram() {
+    let answer = document.getElementById('anagram').value;
+    let word = document.getElementById('word').innerHTML;
+
+    let alert = "";
+    let alertDiv = document.getElementById('alert');
+
+    if (answer == word) {
+        alert = "Same word, loser";
+    } else {
+
+        let wordArray = word.split('');
+        let answerArray = answer.split('');
+
+
+
+        if (wordArray.length != answerArray.length) {
+            alert = "Try again!";
+        } else {
+
+            wordArray.sort();
+            answerArray.sort();
+
+            let finalWord = wordArray.join();
+            let finalAnswer = answerArray.join();
+
+            if (finalWord != finalAnswer) {
+                alert = "Try again!"
+            } else {
+                alert = "Great job!"
+            }
+        }
+    }
+
+        alertDiv.innerHTML = alert;
+
+
+    
+}
